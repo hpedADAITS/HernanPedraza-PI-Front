287ed27c82d9c763810d0219998097824f60918c
Hernan Pedraza <hpedtor0x@adaits.es>
2026-04-15 13:48:41 +0200

    Linting error

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  semi: true,
+  trailingComma: 'all',
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 80,
+  endOfLine: 'lf',
+};
